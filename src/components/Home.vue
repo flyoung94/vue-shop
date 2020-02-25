cbdcb9e3a39d1db23d12483b2662ae18cfe25fdd
flyoung94 <834677043@qq.com>
2020-02-25 17:03:27 +0800

    user

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,117 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <!-- 头部区 -->
+    <el-header>
+      <div @click="goHome">
+        <img src="../assets/heima.png" />
+        <span>后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu background-color="#545c64" text-color="#fff" active-text-color="#409bff" unique-opened router :default-active="activePath">  
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id+''" v-for='item in menulist' :key='item.id'>
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item :index="'/'+subItem.path" v-for="subItem in item.children" :key='subItem.id' @click="saveNavState('/' + subItem.path)">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 主体区  -->
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      menulist: [],  // 左侧菜单数据
+      iconsObj: {    // 左侧菜单图标
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      activePath: ''
+    }
+  },
+  created() {
+    this.getMenuList();
+    this.activePath = window.sessionStorage.getItem('activePath');
+  },
   methods: {
     logout() {
       window.sessionStorage.clear();
       this.$router.push("/login");
+    },
+    async getMenuList() {
+      const {data: res} = await this.$http.get('menus');
+      if(res.meta.status != 200) 
+        return this.$message.error(res.meta.msg);
+      this.menulist = res.data;
+    },
+    saveNavState(activePath) {
+      window.sessionStorage.setItem('activePath', activePath); //保存到session
+      this.activePath = activePath;
+    },
+    goHome() {
+      this.$router.push("/welcome");
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.home-container {
+  height: 100%;
+  width: 100%;
+}
+.el-header {
+  background-color: #373d41; //#31859C
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;  // 边框对齐 
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+const routerPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch(error=> error)
+}
 // 导入全局css样式
 import '../assets/css/global.css'
 // 导入字体库
@@ -9,9 +13,15 @@ import axios from 'axios'
 // 登录组件
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import User from '../components/user/Users.vue'
 
 // 配置请求的根路径
-axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://192.168.1.100:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 
 
@@ -21,7 +31,15 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home }
+  {
+    path: '/home', 
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: User }
+    ]
+  }
 ]
 
 const router = new VueRouter({
@@ -29,14 +47,14 @@ const router = new VueRouter({
 })
 
 // 挂载路由导航守卫
-router.beforeEach((to,from,next) => {
+router.beforeEach((to, from, next) => {
   // to 要访问的路径
   // from 代表从哪个路径跳转而来的
   // next 表示放行：next()直接放行，next('/xx') 强制跳转
-  if(to.path === '/login') 
+  if (to.path === '/login')
     return next()
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) 
+  if (!tokenStr)
     return next('/login')
   next()
 })
